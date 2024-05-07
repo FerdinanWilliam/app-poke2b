56154b004ddce5e850d5ceb42831b5951f9ce1fd
FerdinanWilliam <ferdinanlopes678@gmail.com>
2024-05-07 09:17:19 -0300

    listagem pokémon

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import pokemon from './assets/pokeapi_256.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://pokeapi.co/" target="_blank">
+          <img src={pokemon} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={pokemon} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -21,6 +23,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Listagem/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
